ee7107a68c36fabf43fc52f756157e279dc3cc52
Ojunsun <98933734+Ojunsun@users.noreply.github.com>
2022-08-03 20:13:30 +0900

    Player HP
    Player
    hp bar

---
+++ Assets/02.Scripts/PlayerHPbar/UI.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI : MonoBehaviour
+{
+    [SerializeField]
+    private Slider hpbar;
+    float imsi;
+
+    private float maxHp = 100;
+    private float curHp = 100;
+
+    private void Start()
+    {
+        hpbar.value = (float)curHp / (float)maxHp;
+        imsi = (float)curHp / (float)maxHp;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (curHp > 0)
+            {
+                curHp -= 10;
+            }
+            else
+            {
+                curHp = 0;
+            }
+            imsi = (float)curHp / (float)maxHp; 
+        }
+
+        HandleHp();
+    }
+
+    private void HandleHp()
+    {
+        hpbar.value = Mathf.Lerp(hpbar.value, imsi, Time.deltaTime * 10);
+    }
+}
